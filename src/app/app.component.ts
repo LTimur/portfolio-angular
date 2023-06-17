033165a585aa5df4c0cfc051fe7afdb3066cc826
LTimur <ledenev-timur@ya.ru>
2023-06-17 13:26:34 +0500

    add projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-angular';
+
+  projects = [
+    {
+      image: '',
+      title: 'Hackernews clone',
+      github: 'https://github.com/LTimur/pet-react-ts-hackernews',
+      demo: 'https://hackernewsclonereact.netlify.app/'
+    },
+    {
+      image: '',
+      title: 'Todolist',
+      github: 'https://github.com/LTimur/pet-react-todolist',
+      demo: 'https://pet-react-todolist.netlify.app/'
+    },
+    {
+      image: '',
+      title: 'Landing for educational platform',
+      github: 'https://github.com/LTimur/pet-landing-ementor',
+      demo: 'https://ltimur.github.io/pet-landing-ementor/'
+    }
+  ]
 }
